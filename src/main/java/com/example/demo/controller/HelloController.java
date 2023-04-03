da8005cd01f89cb98e9f844e889d8d46ca493715
shan-rui <shan.rui@plus-automation.com>
2023-04-03 18:32:55 +0900

    init

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    public static Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/abc")
+    public String hello () {
+        log.info(new Date().toLocaleString() + ":hello world");
+        return "hello abc";
+    }
+}
